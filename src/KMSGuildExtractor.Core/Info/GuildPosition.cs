d63737e2cf402391938cad708532d1b99bd936df
Restyled.io <commits@restyled.io>
2020-08-05 21:34:14 +0000

    Restyled by astyle

--- src/KMSGuildExtractor.Core/Info/GuildPosition.cs
+++ src/KMSGuildExtractor.Core/Info/GuildPosition.cs
@@ -1,20 +1,20 @@
 namespace KMSGuildExtractor.Core.Info
 {
-    public enum GuildPosition
-    {
-        /// <summary>
-        /// 길드 마스터
-        /// </summary>
-        Owner,
+public enum GuildPosition
+{
+    /// <summary>
+    /// 길드 마스터
+    /// </summary>
+    Owner,
 
-        /// <summary>
-        /// 길드 부마스터
-        /// </summary>
-        Staff,
+    /// <summary>
+    /// 길드 부마스터
+    /// </summary>
+    Staff,
 
-        /// <summary>
-        /// 길드원
-        /// </summary>
-        Member
-    }
+    /// <summary>
+    /// 길드원
+    /// </summary>
+    Member
+}
 }
