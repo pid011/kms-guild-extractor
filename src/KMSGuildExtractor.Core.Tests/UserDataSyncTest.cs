52b574041ad92afc36bd3e895d184b0090600e1a
Sepi <deet1022@gmail.com>
2020-08-06 09:24:33 +0900

    Add request user data sync

---
+++ src/KMSGuildExtractor.Core.Tests/UserDataSyncTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using KMSGuildExtractor.Core.Requester;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KMSGuildExtractor.Core.Tests
+{
+    [TestClass]
+    public class UserDataSyncTest : WebParsingTest
+    {
+        [TestMethod]
+        public void UserDataSyncNotFound1()
+        {
+            UserDataRequester.SyncData check = WaitForSyncing("일이삼사오육칠");
+
+            Assert.AreEqual(check.Error, true);
+            Assert.AreEqual(check.Message, "캐릭터를 찾을 수 없습니다.");
+        }
+
+        [TestMethod]
+        public void UserDataSyncNotFound2()
+        {
+            UserDataRequester.SyncData check = WaitForSyncing("캡틴이름뮈헤");
+
+            Assert.AreEqual(check.Error, true);
+            Assert.AreEqual(check.Message, "갱신에 실패했습니다. (캐릭터를 찾을 수 없습니다.)");
+        }
+
+        [TestMethod]
+        public void UserDataSyncNotFound3()
+        {
+            UserDataRequester.SyncData check = WaitForSyncing("캡틴이름뭐해");
+
+            Assert.AreEqual(check.Done, true);
+        }
+
+        private UserDataRequester.SyncData WaitForSyncing(string name)
+        {
+            while (true)
+            {
+                Task<UserDataRequester.SyncData> data = UserDataRequester.GetUserSyncData(name);
+                Task.WaitAll(data);
+                if (data.Result.Error == true || data.Result.Done == true)
+                {
+                    return data.Result;
+                }
+                Task.WaitAll(Task.Delay(data.Result.Interval));
+            }
+        }
+    }
+}
---
+++ src/KMSGuildExtractor.Core/Requester/UserDataRequester.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KMSGuildExtractor.Core.Requester
+{
+    public class UserDataRequester : BaseRequester
+    {
+        private const string UserDataUrl = "https://maple.gg/u/{0}";
+        private const string UserSyncUrl = UserDataUrl + "/sync";
+
+        public static async Task<SyncData> GetUserSyncData(string name)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, string.Format(UserSyncUrl, name));
+            using HttpResponseMessage response = await s_client.SendAsync(request);
+
+            byte[] json = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<SyncData>(json.AsSpan());
+        }
+
+        public class SyncData
+        {
+            [JsonPropertyName("error")]
+            public bool Error { get; set; }
+
+            [JsonPropertyName("done")]
+            public bool Done { get; set; }
+
+            [JsonPropertyName("message")]
+            public string Message { get; set; }
+
+            [JsonPropertyName("interval")]
+            public int Interval { get; set; }
+        }
+    }
+}
