e6e90d0a05c12ac6863625a5c83feb89cc70fc52
Sepi <deet1022@gmail.com>
2020-08-15 14:43:02 +0900

    Add Update util class

---
+++ src/KMSGuildExtractor.Core/Requester/GitHubRequester.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KMSGuildExtractor.Core.Requester
+{
+    internal class GitHubRequester : BaseRequester
+    {
+        private const string LatestReleaseLink = "https://api.github.com/repos/pid011/kms-guild-extractor/releases/latest";
+
+        public static async Task<ReleaseData> GetLastReleaseAsync(CancellationToken cancellationToken = default)
+        {
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, LatestReleaseLink);
+            request.Headers.Add("User-Agent", "kms-guild-extractor");
+            using HttpResponseMessage response = await s_client.SendAsync(request, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            using Stream jsonStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<ReleaseData>(jsonStream, cancellationToken: cancellationToken);
+        }
+
+        public class ReleaseData
+        {
+            [JsonPropertyName("html_url")]
+            public string Url { get; set; }
+
+            [JsonPropertyName("tag_name")]
+            public string TagName { get; set; }
+        }
+    }
+}
---
+++ src/KMSGuildExtractor.Core/Utils/Update.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using KMSGuildExtractor.Core.Requester;
+
+namespace KMSGuildExtractor.Core.Utils
+{
+    public static class Update
+    {
+        public static async Task<(bool compare, string url)> CompareVersionAsync(Version target,
+                                                                                 CancellationToken cancellationToken = default)
+        {
+            if (target is null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            string versionString = $"v{target.ToString(3)}";
+            GitHubRequester.ReleaseData data = await GitHubRequester.GetLastReleaseAsync(cancellationToken);
+            return data is null
+                ? (false, null)
+                : (versionString == data.TagName, data.Url);
+        }
+    }
+}
