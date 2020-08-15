f7c992e274717e63f37d5d2d3cfba84cb6797245
Sepi <deet1022@gmail.com>
2020-08-15 09:05:09 +0900

    Format code

--- src/KMSGuildExtractor.Core.Tests/GuildDetailParsingTest.cs
+++ src/KMSGuildExtractor.Core.Tests/GuildDetailParsingTest.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
--- src/KMSGuildExtractor/ViewModel/MainWindowViewModel.cs
+++ src/KMSGuildExtractor/ViewModel/MainWindowViewModel.cs
@@ -1,7 +1,5 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
