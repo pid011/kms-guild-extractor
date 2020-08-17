30e0e90ee03aa0f76764c2fa0fb351cc77e6bf59
Sepi <deet1022@gmail.com>
2020-08-17 11:15:34 +0900

    Add MemberList

--- src/KMSGuildExtractor/ViewModel/DataLoadViewModel.cs
+++ src/KMSGuildExtractor/ViewModel/DataLoadViewModel.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 using KMSGuildExtractor.Core;
 
 namespace KMSGuildExtractor.ViewModel
 {
-    public class DataLoadViewModel : BindableBase
+    internal class DataLoadViewModel : BindableBase
     {
         public Visibility InitializeLoading
         {
@@ -17,11 +21,22 @@ namespace KMSGuildExtractor.ViewModel
             set => SetProperty(ref _initializeLoading, value, nameof(InitializeLoading));
         }
 
+        public Visibility MemberListVisivility
+        {
+            get => _memberListVisivility;
+            set => SetProperty(ref _memberListVisivility, value, nameof(MemberListVisivility));
+        }
+
+        public ObservableCollection<ListViewData> MemberList { get; } = new ObservableCollection<ListViewData>();
+
         private readonly MainWindowViewModel _main;
         private readonly Guild _guild;
 
         private Visibility _initializeLoading;
+        private Visibility _memberListVisivility;
+
         private CancellationTokenSource _guildMemberLoadCancellation;
+        private ConcurrentQueue<(int number, User data)> _userWorkQueue;
 
         public DataLoadViewModel(MainWindowViewModel main, Guild guildData)
         {
@@ -36,9 +51,23 @@ namespace KMSGuildExtractor.ViewModel
             _guildMemberLoadCancellation = new CancellationTokenSource();
             try
             {
+                MemberListVisivility = Visibility.Collapsed;
                 InitializeLoading = Visibility.Visible;
+
                 await _guild.LoadGuildMembersAsync(_guildMemberLoadCancellation.Token);
+
                 InitializeLoading = Visibility.Collapsed;
+                MemberListVisivility = Visibility.Visible;
+
+                foreach ((GuildPosition position, User user) in _guild.Members)
+                {
+                    await Application.Current.Dispatcher.InvokeAsync(() => MemberList.Add(new ListViewData
+                    {
+                        Name = user.Name,
+                        Position = position.ToLocalizedString(),
+                        Status = "준비됨"
+                    }));
+                }
             }
             catch (ParseException)
             {
@@ -48,6 +77,13 @@ namespace KMSGuildExtractor.ViewModel
             {
                 return;
             }
+        }
+
+        internal class ListViewData
+        {
+            public string Name { get; set; }
+            public string Position { get; set; }
+            public string Status { get; set; }
         }
     }
 }
